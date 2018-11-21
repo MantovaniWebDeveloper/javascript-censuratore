1b91dd8ad2b744935654473c87f273a4a39eea4e
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-21 14:01:57 +0100

    init e logica scritta in italiano

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//creo la frase
+var fraseUtente = "Ciao sono Fernando Alonso, ed il prossimo anno guiderò in Indycar";
+// salvo in un array le parole proibite
+var paroleProibite = ["Ciao","guiderò"];
+
+//navigo la frase e nel mentre
+//confronto le parole della frase con
+// le parole proibite
+
+//se una parola della frase è proibita
+// la parola deve essere sostitutia con xxx
