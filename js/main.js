027c50c755f4c2ee3ead98677340ceeb9f8c252f
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-21 17:20:29 +0100

    versione funzionante con anche conteggio censure e numero parole

--- js/main.js
+++ js/main.js
@@ -5,17 +5,30 @@ var paroleProibite = ["Fernando","guiderò","ed"];
 //navigo la frase e nel mentre
 //confronto le parole della frase con
 // le parole proibite
-for (var i = 0; i < fraseUtente.length; i++) {
-  var parole = fraseUtente.split(" ");
-}
+
 
 var contatore = 0;
-while (contatore < paroleProibite.length){
+var paroleCensurate = 0;
+while (contatore < paroleProibite.length) {
   console.log(paroleProibite[contatore]);
   if(fraseUtente.includes(paroleProibite[contatore])){
     fraseUtente = fraseUtente.replace(paroleProibite[contatore], 'xxx');
+    paroleCensurate++;
   }
   contatore ++;
 }
+
+function contaParoleFrase(fraseUtente){
+  var numeroParoleSeparate;
+  var paroleSeparate =fraseUtente.split(" ");
+  console.log(paroleSeparate)
+  numeroParoleSeparate = paroleSeparate.length;
+  return numeroParoleSeparate;
+}
+
+var paroleFraseCont = contaParoleFrase(fraseUtente);
+
 console.log(fraseUtente);
+console.log("Numero parole censurate " +paroleCensurate);
+console.log("paroli totali nella frase " + paroleFraseCont);
 document.writeln(fraseUtente);
